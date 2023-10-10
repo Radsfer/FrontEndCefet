195c520f4103e816b87ebe89ec94e0338b27274c
Rafael Adolfo <rafael.ferreira11.98@gmail.com>
2023-10-10 00:08:55 -0300

    atividade completa

---
+++ cliente-fornecedor/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Alo Mundo!</h1>
+      testando react!!    
+    </div>
+  );
+}
